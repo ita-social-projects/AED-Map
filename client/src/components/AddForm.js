b792b5a7b275b55c3b23b394065c21e2ad07bf34
Ostap Kovbasiuk <ostapkovbasyk@gmail.com>
2020-01-13 13:17:34 +0200

    Additional components created

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
-class Header extends Component {
+class AddForm extends Component {
   render() {
     return <div></div>;
   }
 }
 
-export default Header;
+export default AddForm;
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import Point from "./Point";
 import myClasses from "../styles";
+import Header from "./Header";
+import ItemList from "./ItemList";
+import AddForm from "./AddForm";
+import Filter from "./Filter";
 
 class Sidebar extends Component {
   render() {
     return (
       <div className={myClasses.sidebarStyle}>
-        {this.props.points.map(point => (
-          <Point
-            key={point.id}
-            point={point}
-            flyToPin={this.props.flyToPin}
-          />
-        ))}
+        <Header />
+        <ItemList
+          points={this.props.points}
+          flyToPin={this.props.flyToPin}
+        />
+        <AddForm />
+        <Filter />
       </div>
     );
   }
