ba49d1796c54ddd36e3cbadaf2002dba4bd0a44e
Yaroslav Vendysh <Yro.Vendysh@gmail.com>
2020-01-13 19:02:17 +0200

    Updated server.js and added folder/file for defibrillatorRoutes on backend

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const path = require('path')
+
+// IMPORT MODELS
+require('./models/Defibrillator');
+
 const app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/defibrillatorDB`,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+);
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('client/build'));
 
-MongoClient.connect('mongodb://localhost:27017',
-    { useUnifiedTopology: true },
-    function(err, database){
-    if (err) {
-        console.log(err)
-    }  
-    app.listen(3012, function() {
-        console.log('API app started')
-    })
-})
+// IMPORT ROUTES
+require('./routes/defibrillatorRoutes')(app);
 
-app.get('/', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
 });
