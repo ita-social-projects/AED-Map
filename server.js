8ffaf940325ef121be3a57208a268a9609b5dee1
unknown <vtelyachy@gmail.com>
2020-02-27 19:56:08 +0200

    fixing port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ mongoose
     console.log(`db error ${err.message}`);
   });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(
     path.resolve(__dirname, 'client', 'build', 'index.html')
   );
