f8cf2b1e5b410d0a0c5b48d4a7c12a9e1d2a44c4
SaraTec <cosmo.epik@gmail.com>
2020-03-04 12:21:02 +0200

    added owner field to redux

--- client/src/modules/Sidebar/components/AddForm/AddItRedux.js
+++ client/src/modules/Sidebar/components/AddForm/AddItRedux.js
@@ -1,10 +1,11 @@
 ﻿/* eslint-disable */
-const AddItRedux = (data,createDef,id)=> {
+const AddItRedux = (data,createDef,_id,owner)=> {
   
   const set_obj ={
     ...data,
     actual_date:data.actualDate,
-    _id: id,
+    _id,
+    owner,
     location: { type: 'Point', coordinates:data.coordinates },
     language: 'Україномовний',
     phone: [data.phone],
