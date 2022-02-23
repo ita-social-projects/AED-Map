dc3f693d53b1f416046c93ce680e50b77f92c36e
volodymyryaniv <erjaniv@ukr.net>
2022-02-23 13:31:24 +0200

    Added validation reg exp to `title`and `place`

--- client/src/shared/Form/validator.js
+++ client/src/shared/Form/validator.js
@@ -1,23 +1,32 @@
 ﻿import * as Yup from 'yup';
 
 const FormValidation = Yup.object().shape({
-  address: Yup.string().required('Поле обов\'язкове'),
-  title: Yup.string().required('Поле обов\'язкове'),
+  address: Yup.string().required("Поле обов'язкове"),
+  title: Yup.string()
+     .required("Поле обов'язкове")
+     .matches(
+        /[^\S]*([A-zА-я]|\d)+/gim,
+        'Формат вводу неправильний,поле повинно містити букви'
+     ),
   phone: Yup.array().of(
-    Yup.string()
-      .required('Формат номеру неправильний')
-      .matches(
-        // eslint-disable-next-line no-useless-escape
-        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[(]?[0-9]{2}[)]?[-\s\.]?[0-9]{3}[-\s)\.]?[0-9]{2}[-\s)\.]?[0-9]{2}$/im,
-        'Формат номеру неправильний'
-      )
+     Yup.string()
+        .required('Формат номеру неправильний')
+        .matches(
+           // eslint-disable-next-line no-useless-escape
+           /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[(]?[0-9]{2}[)]?[-\s\.]?[0-9]{3}[-\s)\.]?[0-9]{2}[-\s)\.]?[0-9]{2}$/im,
+           'Формат номеру неправильний'
+        )
   ),
-  // availableFrom: Yup.string().required('Поле обов\'язкове'),
-  storage_place: Yup.string().required('Поле обов\'язкове'),
+  storage_place: Yup.string()
+     .required("Поле обов'язкове")
+     .matches(
+        /[^\S]*([A-zА-я]|\d)+/gim,
+        'Формат вводу неправильний,поле повинно містити букви'
+     ),
   floor: Yup.number()
-    .required('Поле обов\'язкове')
-    .min(0, 'Значення поля не може бути від\'ємне')
-    .max(20, 'Значення поля не може бути вище 20')
+     .required("Поле обов'язкове")
+     .min(0, "Значення поля не може бути від'ємне")
+     .max(20, 'Значення поля не може бути вище 20'),
 });
 
 export default FormValidation;
