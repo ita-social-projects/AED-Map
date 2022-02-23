d276fd8c590dfbb485832d64de3c6217c4f87245
volodymyryaniv <erjaniv@ukr.net>
2022-02-23 15:13:53 +0200

    Fixed quotes

--- client/src/shared/Form/validator.js
+++ client/src/shared/Form/validator.js
@@ -1,9 +1,9 @@
 ﻿import * as Yup from 'yup';
 
 const FormValidation = Yup.object().shape({
-  address: Yup.string().required("Поле обов'язкове"),
+  address: Yup.string().required('Поле обов\'язкове'),
   title: Yup.string()
-    .required("Поле обов'язкове")
+    .required('Поле обов\'язкове')
     .matches(
       /[^\S]*([A-zА-я]|\d)+/gim,
       'Формат вводу неправильний,поле повинно містити букви'
@@ -12,20 +12,20 @@ const FormValidation = Yup.object().shape({
     Yup.string()
       .required('Формат номеру неправильний')
       .matches(
-          // eslint-disable-next-line no-useless-escape
+        // eslint-disable-next-line no-useless-escape
         /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[(]?[0-9]{2}[)]?[-\s\.]?[0-9]{3}[-\s)\.]?[0-9]{2}[-\s)\.]?[0-9]{2}$/im,
         'Формат номеру неправильний'
       )
   ),
   storage_place: Yup.string()
-    .required("Поле обов'язкове")
+    .required('Поле обов\'язкове')
     .matches(
       /[^\S]*([A-zА-я]|\d)+/gim,
       'Формат вводу неправильний,поле повинно містити букви'
     ),
   floor: Yup.number()
-    .required("Поле обов'язкове")
-    .min(0, "Значення поля не може бути від'ємне")
+    .required('Поле обов\'язкове')
+    .min(0, 'Значення поля не може бути від\'ємне')
     .max(20, 'Значення поля не може бути вище 20'),
 });
 
